8e2c152a1e8c1dc61bf6bf74294fc1ea25a5e449
Dima Ivanov <ivanovdimat5@gmail.com>
2023-01-07 17:20:27 +0300

    Решение второй задачи

---
+++ Task_2/Program.Task_2.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int a = 45;
+int b = 57;
+int c = 85;
+
+int max = a;
+
+    if (b > max) max = b;
+    if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
