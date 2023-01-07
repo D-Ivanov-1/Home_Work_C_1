85bb01b24652527f26d66d221db9bb501752b991
Dima Ivanov <ivanovdimat5@gmail.com>
2023-01-07 17:21:55 +0300

    Решение четвёртой задачи

---
+++ Task_4/Program.Task_4.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int N = 8;
+int[] numbers = {1, N}; // 1 2 3 ... N
+int index = 1;
+while (index <= N)
+{
+    if (index % 2 == 0)
+    Console.Write($"{index}, ");
+    index++;
+}
