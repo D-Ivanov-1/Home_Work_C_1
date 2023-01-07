105c53c45bd82eb1aaed11ca768f9ab1ab14a77d
Dima Ivanov <ivanovdimat5@gmail.com>
2023-01-07 17:19:49 +0300

    Решение первой задачи

---
+++ Task_1/Program.Task_1.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int a = 5;
+int b = 7;
+
+
+int max = a;
+int min = a;
+
+    if (b > max) max = b;
+    if (b < min) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
